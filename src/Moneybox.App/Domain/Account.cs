f9fde68ca5d18fea1cd81e5197a3eab1b3e91ee5
the-old-goose <theeoldgoose@gmail.com>
2019-08-07 12:56:07 +0100

    Added logic to WithdrawMoney, added methods to Account

--- src/Moneybox.App/Domain/Account.cs
+++ src/Moneybox.App/Domain/Account.cs
@@ -6,12 +6,13 @@ namespace Moneybox.App
     {
         private const decimal PayInLimit = 4000m;
         private const decimal NotificationThreshold = 500m;
+        private const decimal WithdrawLimit = 12000m;
 
         public Guid Id { get; set; }
 
         public User User { get; set; }
 
-        //These properties should never be set outside this class
+        //These properties should never be set outside this class therefore I have added private accessibility to the properties setter.
         public decimal Balance { get; private set; }
 
         public decimal Withdrawn { get; private set; }
@@ -41,15 +42,31 @@ namespace Moneybox.App
             return ((this.Balance - amount) < 500m);   
         }
 
+        public void CheckWithdrawalLimit(decimal amount)
+        {
+            decimal withdrawn = this.Withdrawn + amount;
+
+            if (withdrawn > Account.WithdrawLimit)
+            {
+                throw new InvalidOperationException("Account withdrawal limit reached");
+            }
+        }
+
         public bool ApproachingPayInLimit(decimal amount)
         {
             return (Account.PayInLimit - (this.PaidIn + amount) < Account.NotificationThreshold);
             
         }
 
+        public bool ApproachingWithdrawnLimit(decimal amount)
+        {
+            return (Account.WithdrawLimit - (this.PaidIn + amount) < Account.NotificationThreshold);
+         
+        }
+
         public bool Transfer(Account to,decimal amount)
         {
-            //Its likely more logic will be added once the app expands so I have wrapped in try catch and will return a bool to the calling function.
+            //Its likely more logic will be added once the app expands so I have wrapped this in a try catch and will return a bool to the calling function.
             try
             {
                 this.Balance -= amount;
@@ -63,6 +80,22 @@ namespace Moneybox.App
             catch (Exception failedTransfer)
             { 
                 failedTransfer.ToString();
+                return false;
+            }
+        }
+
+        public bool Withdraw(decimal amount)
+        {
+            //Its likely more logic will be added once the app expands so I have wrapped this in a try catch and will return a bool to the calling function.
+            try
+            {
+                this.Balance -= amount;
+                this.Withdrawn += amount;
+                return true;
+            }
+            catch (Exception failedWithdrawal)
+            {
+                failedWithdrawal.ToString();
                 return false;
             }
         }
--- src/Moneybox.App/Features/WithdrawMoney.cs
+++ src/Moneybox.App/Features/WithdrawMoney.cs
@@ -6,8 +6,8 @@ namespace Moneybox.App.Features
 {
     public class WithdrawMoney
     {
-        private IAccountRepository accountRepository;
-        private INotificationService notificationService;
+        private readonly IAccountRepository accountRepository;
+        private readonly INotificationService notificationService;
 
         public WithdrawMoney(IAccountRepository accountRepository, INotificationService notificationService)
         {
@@ -17,7 +17,35 @@ namespace Moneybox.App.Features
 
         public void Execute(Guid fromAccountId, decimal amount)
         {
-            // TODO:
+            //As the type is obvious and assuming the correct checks are implemented in 'accountRepository', declaring 'from' as a var is fine. 
+            var from = this.accountRepository.GetAccountById(fromAccountId);
+        
+            bool lowFundsFlag=false;
+            
+            from.CheckFunds(amount);
+
+            from.CheckWithdrawalLimit(amount);
+
+            if (from.LowFundsAfterTransaction(amount))
+            {
+                lowFundsFlag = true;
+            }
+
+            //The same logic is followed with TransferMoney
+            if(from.Withdraw(amount))
+            {
+                Update();
+
+                if(lowFundsFlag)
+                {
+                    this.notificationService.NotifyFundsLow(from.User.Email);
+                }
+            }
+        }
+
+        private void Update()
+        {
+            this.accountRepository.Update(from);
         }
     }
 }
